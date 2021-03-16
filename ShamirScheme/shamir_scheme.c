4ad148d1c83168e533a0cbeed302a1f08c9e948b
avduckart <53112970+avduckart@users.noreply.github.com>
2021-03-16 12:36:00 +0300

    Update shamir_scheme.c

--- ShamirScheme/shamir_scheme.c
+++ ShamirScheme/shamir_scheme.c
@@ -14,7 +14,7 @@ void calc_a0(share_data_t*);
 void calc_a1(share_data_t*);
 void calc_a2(share_data_t*);
 void calc_term(restore_data_t*);
-void run_calc(thread_t*, share_data_t*);
+void run_calc(thread_t*, const share_data_t*);
 void zero_part(part_t*);
 void zero_parts(part_t*);
 int is_ready_to_write_parts(part_t*);
