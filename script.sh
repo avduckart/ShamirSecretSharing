675ee9d8b0b555d446d1cc352e00f1c24b365906
avduckart <53112970+avduckart@users.noreply.github.com>
2021-03-24 14:03:20 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-cmake .
-cmake --build .
-cmake --build . --target test
-cmake --build . --target package
+cmake -DCMAKE_BUILD_TYPE=Debug .
+cmake -DCMAKE_BUILD_TYPE=Debug --build .
+cmake -DCMAKE_BUILD_TYPE=Debug --build . --target test
+cmake -DCMAKE_BUILD_TYPE=Debug --build . --target package
 
 valgrind --tool=memcheck --leak-check=yes build/Release/shamir_test
