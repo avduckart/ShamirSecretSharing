d087440ebd4240d322347c993eab1faac1cd835a
avduckart <53112970+avduckart@users.noreply.github.com>
2021-03-25 12:48:37 +0300

    Update multithreading.h

--- ShamirScheme/multithreading.h
+++ ShamirScheme/multithreading.h
@@ -28,7 +28,7 @@
     }
 
     #include <asm/errno.h>
-    #define create_thread(thread, func, data) pthread_create(&thread, NULL, func, data)
+    #define create_thread(thread, func, data) pthread_create(thread, NULL, func, data)
     #define join(threads) unix_join(threads)
     #define close_thread(thread) pthread_cancel(thread)
     #define mutex_t pthread_mutex_t
