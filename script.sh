82bc660b0fb841d97abe58468ca08d38f7195c4f
avduckart <53112970+avduckart@users.noreply.github.com>
2021-03-31 15:23:09 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,13 +2,6 @@
 
 cmake .
 cmake --build .
-cmake --build . --target test
-cmake --build . --target package
 
 valgrind --tool=memcheck --leak-check=yes --track-origins=yes -s build/Release/shamir_test
 valgrind --tool=helgrind build/Release/shamir_test
-
-rm -rf coverage
-mkdir coverage
-build/Release/shamir_test
-gcovr --filter source/ headers/ --print-summary --html-details -o coverage/index.html
