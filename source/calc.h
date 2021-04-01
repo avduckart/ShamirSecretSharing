84e9f88ab3cff41aa197b8b4cea413a6e419864d
avduckart <53112970+avduckart@users.noreply.github.com>
2021-04-01 17:25:49 +0300

    Update calc.h

--- source/calc.h
+++ source/calc.h
@@ -3,6 +3,11 @@
 #ifndef __CALC_H__
 #define __CALC_H__
 
+#define secure_alloc(type, var, func)   type * var = func(); if(!var) { data->success = 0xF0; return;}
+#define secure_free(func, var)          if(var) func(var);
+#define check(cond, status)             if(cond) return status;
+#define is_null(ptr, status)            check(!ptr, status);
+
 void calc_a0(share_data_t*);
 void calc_a1(share_data_t*);
 void calc_a2(share_data_t*);
