4b2df9c37f722b4a44b4d26f45facaf08b93e36d
avduckart <53112970+avduckart@users.noreply.github.com>
2021-04-06 18:23:46 +0300

    Update macros.h

--- testing/macros.h
+++ testing/macros.h
@@ -5,6 +5,6 @@
 
 #define BN_secure_alloc(var)        var = BN_new(); ASSERT_TRUE(var)
 #define BN_secure_assign(var,val)   BN_secure_alloc(var); ASSERT_TRUE(BN_set_word(var, val))
-#define BN_secure_free(var)         if(var) BN_free(var)
+#define BN_secure_free(var)         if(var) {BN_free(var); var = NULL;}
 
 #endif //__MACROS_H__
